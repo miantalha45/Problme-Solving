1dc73bc582fc183230c4cc35ac9096d0580d22f8
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-04-25 11:46:48 +0500

    Time: 4 ms (43.29%) | Memory: 49.4 MB (49.12%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        HashSet<string> seen = new HashSet<string>();
+
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                char ch = board[i][j];
+                if(ch != '.')
+                {
+                    string rowKey = $"row{i}-{ch}";
+                    string colKey = $"col{j}-{ch}";
+                    string squareKey = $"box{i / 3}{j / 3}-{ch}";
+
+                    if(!seen.Add(rowKey) || !seen.Add(colKey) || !seen.Add(squareKey))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
