66fce548d7d188b7876fa7b144cc5d270bea082c
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-05-30 14:24:42 +0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    unordered_map<int, int> memo;
     int fib(int n) {
         if(n <= 1)
             return n;
         
-        return fib(n - 1) + fib(n - 2);
+        if(memo.count(n) > 0)
+        {
+            return memo[n];
+        }
+        memo[n] = fib(n - 1) + fib(n - 2);
+        return memo[n];
     }
 };
