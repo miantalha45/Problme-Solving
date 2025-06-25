d28b41e3e67c819995969853b50d32f414b9b05b
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-06-25 14:27:31 +0500

    Time: 1 ms (91.78%) | Memory: 53.7 MB (71.87%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cs
+++ 2-add-two-numbers/add-two-numbers.cs
@@ -11,51 +11,23 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode p1 = l1;
-        ListNode p2 = l2;
+        return Add(l1, l2, 0);
+    }
 
-        ListNode dummy = new ListNode(0);
-        ListNode sum = dummy;
-        int carry = 0;
+    private ListNode Add(ListNode l1, ListNode l2, int carry)
+    {
+        if(l1 == null && l2 == null && carry == 0)
+            return null;
+        
+        int val1 = l1?.val ?? 0;
+        int val2 = l2?.val ?? 0;
 
-        while(p1 != null || p2 != null)
-        {
-            if(p1 != null && p2 != null)
-            {
-                int s = (p1.val + p2.val + carry);
-                carry = s / 10;
-                int digit = s % 10;
-                sum.next = new ListNode(digit);
-                sum = sum.next;
-                p1 = p1.next;
-                p2 = p2.next;
-            }
-            else if(p1 != null)
-            {
-                int s = (p1.val + carry);
-                carry = s / 10;
-                int digit = s % 10;
-                sum.next = new ListNode(digit);
-                sum = sum.next;
-                p1 = p1.next;
-            }
-            else
-            {
-                int s = (p2.val + carry);
-                carry = s / 10;
-                int digit = s % 10;
-                sum.next = new ListNode(digit);
-                sum = sum.next;
-                p2 = p2.next;
-            }
-        }
+        int sum = val1 + val2 + carry;
+        int newDigit = sum % 10;
+        int newCarry = sum / 10;
 
-        if(carry > 0)
-        {
-            sum.next = new ListNode(carry);
-            sum = sum.next;
-        }
-
-        return dummy.next;
+        ListNode node = new ListNode(newDigit);
+        node.next = Add(l1?.next, l2?.next, newCarry);
+        return node;
     }
 }
