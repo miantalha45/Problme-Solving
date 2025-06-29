da64ba9322b1b7e5682bc441cde9c9bf91696a67
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-06-29 12:42:09 +0500

    Time: 11 ms (34.90%) | Memory: 51.4 MB (68.43%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cs
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cs
@@ -1,17 +1,26 @@
 public class Solution {
     public int[] TopKFrequent(int[] nums, int k) {
+        if(nums.Length == 1)
+            return nums;
+
         Dictionary<int, int> map = new Dictionary<int, int>();
 
-        foreach (int num in nums) {
-            if (map.ContainsKey(num)) {
-                map[num]++;
-            } else {
-                map[num] = 1;
+        foreach(int n in nums)
+        {
+            if(map.ContainsKey(n))
+            {
+                map[n]++;
+            }
+            else
+            {
+                map[n] = 1;
             }
         }
 
-        List<int>[] buckets = new List<int>[nums.Length + 1];
-        foreach (var pair in map) {
+       List<int>[] buckets = new List<int>[nums.Length + 1];
+
+        foreach(var pair in map)
+        {
             int freq = pair.Value;
             if (buckets[freq] == null) {
                 buckets[freq] = new List<int>();
@@ -20,8 +29,10 @@ public class Solution {
         }
 
         List<int> res = new List<int>();
-        for (int i = buckets.Length - 1; i >= 0 && res.Count < k; i--) {
-            if (buckets[i] != null) {
+        for(int i = buckets.Length - 1;i >= 0;i--)
+        {
+            if(buckets[i] != null)
+            {
                 res.AddRange(buckets[i]);
             }
         }
