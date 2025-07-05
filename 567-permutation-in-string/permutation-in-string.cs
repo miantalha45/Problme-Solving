3ecb4d4cbe26ea20b23cd7d1a222a971d2f7e36a
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-07-06 03:58:11 +0500

    Time: 12 ms (31.15%) | Memory: 44.1 MB (22.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cs
+++ 567-permutation-in-string/permutation-in-string.cs
@@ -1,43 +1,47 @@
 public class Solution {
     public bool CheckInclusion(string s1, string s2) {
         if (s1.Length > s2.Length) return false;
+        int n = s2.Length;
 
-        var map1 = new Dictionary<char, int>();
-        var map2 = new Dictionary<char, int>();
+        Dictionary<char, int> s1_Map = new();
+        Dictionary<char, int> s2_Map = new();
 
-        for (int i = 0; i < s1.Length; i++) {
-            map1[s1[i]] = map1.GetValueOrDefault(s1[i], 0) + 1;
-            map2[s2[i]] = map2.GetValueOrDefault(s2[i], 0) + 1;
+        for(int i = 0;i < s1.Length;i++)
+        {
+            s1_Map[s1[i]] = s1_Map.GetValueOrDefault(s1[i], 0) + 1;
+            s2_Map[s2[i]] = s2_Map.GetValueOrDefault(s2[i], 0) + 1;
         }
 
-        if (CompareMaps(map1, map2)) return true;
+        if(AreFrequenciesEqual(s1_Map, s2_Map))
+        {
+            return true;
+        } 
 
-        for (int i = s1.Length; i < s2.Length; i++) {
+        for (int i = s1.Length; i < n; i++) {
             char newChar = s2[i];
             char oldChar = s2[i - s1.Length];
 
-            // Add new char
-            map2[newChar] = map2.GetValueOrDefault(newChar, 0) + 1;
+            s2_Map[newChar] = s2_Map.GetValueOrDefault(newChar, 0) + 1;
 
-            // Remove old char
-            if (map2.ContainsKey(oldChar)) {
-                map2[oldChar]--;
-                if (map2[oldChar] == 0) map2.Remove(oldChar);
+            if (s2_Map.ContainsKey(oldChar)) {
+                s2_Map[oldChar]--;
+                if (s2_Map[oldChar] == 0) s2_Map.Remove(oldChar);
             }
 
-            if (CompareMaps(map1, map2)) return true;
+            if (AreFrequenciesEqual(s1_Map, s2_Map)) return true;
         }
 
         return false;
     }
 
-    private bool CompareMaps(Dictionary<char, int> map1, Dictionary<char, int> map2) {
-        if (map1.Count != map2.Count) return false;
+    private bool AreFrequenciesEqual(Dictionary<char, int> map1, Dictionary<char, int> map2)
+{
+    if (map1.Count != map2.Count) return false;
 
         foreach (var kvp in map1) {
             if (!map2.ContainsKey(kvp.Key) || map2[kvp.Key] != kvp.Value) return false;
         }
 
         return true;
-    }
+}
 }
