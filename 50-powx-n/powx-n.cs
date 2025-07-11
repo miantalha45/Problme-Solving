45d00461d238e33ec355b4a0b30c52bb98ebc08c
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-07-11 10:30:51 +0500

    Time: 0 ms (100.00%) | Memory: 29.1 MB (83.96%) - LeetSync

--- 50-powx-n/powx-n.cs
+++ 50-powx-n/powx-n.cs
@@ -1,22 +1,17 @@
 public class Solution {
     public double MyPow(double x, int n) {
-        long power = n;
-        if(power < 0)
-        {
+        long pow = (long)n;
+        if(pow < 0) {
+            pow = -pow;
             x = 1 / x;
-            power = -power;
         }
-        double result = 1;
-        while(power > 0)
-        {
-            if((power & 1) == 1)
-            {
-                result *= x;
-            }
-            x *= x;
-            power >>= 1;
+        if(pow == 0) return 1;
+        
+        double half = MyPow(x, (int)(pow / 2));
+        if(pow % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
         }
-
-        return result;
     }
 }
