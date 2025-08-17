dbb23ffa1c3da225a5376c0b381a2efc3e97a242
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-08-17 09:27:34 +0500

    Time: 3 ms (71.59%) | Memory: 15.6 MB (50.39%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
-
-        function<void(string, int, int)> backtrack = [&](string current, int open, int close) {
-            if (current.length() == n * 2) {
-                result.push_back(current);
-                return;
-            }
-
-            if (open < n)
-                backtrack(current + '(', open + 1, close);
-
-            if (close < open)
-                backtrack(current + ')', open, close + 1);
-        };
-
-        backtrack("", 0, 0);
-
+        AddParenthesis(result, n, "", 0, 0);
         return result;
+    }
+
+    void AddParenthesis(vector<string>& result, int n, string brace,
+                        int openCount, int closeCount) {
+        if (openCount == n && closeCount == n) {
+            result.push_back(brace);
+        }
+        if (openCount < n) {
+            AddParenthesis(result, n, brace + "(", openCount + 1, closeCount);
+        }
+        if (closeCount < openCount && closeCount < n)
+        {
+            AddParenthesis(result, n, brace + ")", openCount, closeCount + 1);
+        }
     }
 };
