59389dfc043a82f40d50f2cb3728123612186f2d
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-08-23 09:22:05 +0500

    Time: 0 ms (100.00%) | Memory: 112.3 MB (94.18%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+
         if(accumulate(gas.begin(), gas.end(), 0) < accumulate(cost.begin(), cost.end(), 0))
         {
             return -1;
         }
 
-        int currGas = 0;
-        int start = 0;
-        for(int i = 0;i < gas.size(); i++)
+        int n = gas.size();
+        int total = 0;
+        int startStation = 0;
+
+        for(int i = 0;i < n;i++)
         {
-            currGas += (gas[i] - cost[i]);
-            if(currGas < 0)
+            total += (gas[i] - cost[i]);
+            if(total < 0)
             {
-                start = i + 1;
-                currGas = 0;
+                total = 0;
+                startStation = i + 1;
             }
         }
 
-        return start;
+        return startStation;
     }
 };
