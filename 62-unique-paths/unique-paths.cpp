70e68a76153857e1dbd2ad65ab204cca6de41f95
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-10-12 12:30:27 +0500

    Time: 0 ms (100.00%) | Memory: 9.5 MB (11.13%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    map<pair<int, int>, int> map;
     int uniquePaths(int m, int n) {
-        return helper(m, n, 0, 0);
-    }
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
 
-    int helper(int m, int n, int i, int j) {
-        if(i >= m || j >= n) return 0;
-        if(i==m-1 && j==n-1) return 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
 
-        if(map.count({i, j}))   return map[{i, j}];
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
 
-        int rightCount = helper(m, n, i, j+1);
-        int downCount = helper(m, n, i+1, j);
-
-        return map[{i,j}] = rightCount + downCount;
+        return dp[m - 1][n - 1];
     }
 };
