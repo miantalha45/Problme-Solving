d3c5b82b566f2e50797f0e84b26e536289ade2c7
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-10-21 11:19:10 +0500

    Time: 23 ms (69.90%) | Memory: 17.9 MB (83.13%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> memo(amount + 1, -2);
-        return helper(coins, amount, memo);
-    }
-
-    int helper(vector<int>& coins, int amount, vector<int>& memo) {
         if (amount == 0)
             return 0;
         if (amount < 0)
             return -1;
 
-        if (memo[amount] != -2)
-            return memo[amount];
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
 
-        int minCoins = INT_MAX;
-        for (int coin : coins) {
-            int result = helper(coins, amount - coin, memo);
-            if (result != -1) {
-                minCoins = min(minCoins, 1 + result);
+        int coinCount = INT_MAX;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+                }
             }
         }
 
-        return memo[amount] = (minCoins == INT_MAX ? -1 : minCoins);
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
