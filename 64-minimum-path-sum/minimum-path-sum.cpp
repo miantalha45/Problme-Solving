d2b3768c592afbada23126605237570bb398444b
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-11-19 08:40:26 +0500

    Time: 4 ms (15.67%) | Memory: 16.3 MB (57.55%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int traverse(vector<vector<int>>& grid, int i, int j, int n, int m, vector<vector<int>>& dp) {
-        if (i >= n || j >= m)
-            return 1e9;
-
-            if(dp[i][j] != -1) {
-                return dp[i][j];
-            }
-
-        if (i == n - 1 && j == m - 1)
-            return grid[i][j];
-
-        return dp[i][j] = grid[i][j] + min(traverse(grid, i + 1, j, n, m, dp),
-                                traverse(grid, i, j + 1, n, m, dp));
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
         vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        return traverse(grid, 0, 0, n, m, dp);
+        dp[n - 1][m - 1] = grid[n - 1][m - 1];
+
+        for (int j = m - 2; j >= 0; j--)
+            dp[n - 1][j] = grid[n - 1][j] + dp[n - 1][j + 1];
+
+        for (int i = n - 2; i >= 0; i--)
+            dp[i][m - 1] = grid[i][m - 1] + dp[i + 1][m - 1];
+
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = m - 2; j >= 0; j--) {
+                dp[i][j] = grid[i][j] + min(dp[i][j+1], dp[i+1][j]);
+            }
+        }
+
+        return dp[0][0];
     }
 };
