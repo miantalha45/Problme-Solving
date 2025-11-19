7357e3513d6ac162b9ab76be6f04feb611e7dd2f
Talha Amjad <155809113+miantalha45@users.noreply.github.com>
2025-11-19 07:41:10 +0500

    Time: 4 ms (23.50%) | Memory: 52.2 MB (65.35%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,30 +1,11 @@
 class Solution {
-private:
-    bool traverse(vector<int>& nums, int index, vector<int>& dp){
-        if(index >= nums.size()) {
-            return false;
-        }
-
-        if(index == nums.size() - 1) {
-            dp[index] = 1;
-            return true;
-        }
-
-        if(dp[index] != -1) {
-            return dp[index] == 1 ? true : false;
-        }
-
-        for(int i = nums[index]; i > 0; i--) {
-            if(traverse(nums, index + i, dp)) {
-                return true;
-            }
-        }
-        dp[index] = 0;
-        return false;
-    }
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return traverse(nums, 0, dp);
+        int lastGood = nums.size() - 1;
+        for(int i = nums.size() - 2; i >= 0; i--) {
+            if(i + nums[i] >= lastGood) lastGood = i;
+        }
+        if(lastGood == 0) return true;
+        return false;
     }
 };
